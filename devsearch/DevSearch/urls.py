b7b1484d78aaf8cde3cfa744b8a63d1f9927bd6a
OlgaKonieczko <konieczko.olga@gmail.com>
2023-09-17 17:29:34 +0200

    Cleaning up structure

--- devsearch/DevSearch/urls.py
+++ devsearch/DevSearch/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for devsearch project.
+URL configuration for DevSearch project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
