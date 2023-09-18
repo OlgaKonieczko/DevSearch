c1a181225d2edf6762cd226b4165dd0c2d732a99
OlgaKonieczko <konieczko.olga@gmail.com>
2023-09-18 18:40:08 +0200

    Added basic Views and Templates. DB creation started.

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def projects(request):
+    page = 'projects'
+    number = 11
+    context = {'page' : page, 'number' : number}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    return render(request, 'projects/single-project.html')
+
